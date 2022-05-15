53aa957d0a1d7e93e354ad857d220d349e72aa22
Nurhafifah20 <nurhafifah265@gmail.com>
2022-05-15 09:03:59 +0700

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:kebapp/view/auth/login.dart';
+import 'package:kebapp/view/auth/lupa_katasandi.dart';
 import 'package:kebapp/view/auth/register.dart';
 import 'package:kebapp/view/auth_user/ui/keranjang/keranjang.dart';
+import 'package:kebapp/view/auth_user/ui/menu/detail_kebab.dart';
 import 'package:kebapp/view/auth_user/ui/menu/menu.dart';
+import 'package:kebapp/view/auth_user/ui/profil/alamat.dart';
+import 'package:kebapp/view/auth_user/ui/profil/alamat_baru.dart';
+import 'package:kebapp/view/auth_user/ui/profil/google_maps.dart';
 import 'package:kebapp/view/auth_user/ui/profil/profil.dart';
 import 'package:kebapp/view/auth_user/ui/profil/riwayat.dart';
 import 'package:kebapp/view/auth_user/ui/profil/tentang_kami.dart';
@@ -31,15 +36,27 @@ class MyApp extends StatelessWidget {
         SplashScreen.routeName: (context) => const SplashScreen(),
         LoginPage.routeName: (context) => LoginPage(),
         RegisterPage.routeName: (context) => RegisterPage(),
-        MyBottomNavigationBar.routeName: (context) => MyBottomNavigationBar(),
+        LupaKataSandi.routeName: (context) => LupaKataSandi(),
+        MyBottomNavigationBar.routeName: (context) =>
+            const MyBottomNavigationBar(),
         Menu.routeName: (context) => Menu(),
         Keranjang.routeName: (context) => Keranjang(),
         MyProfile.routeName: (context) => MyProfile(),
         UbahAkun.routeName: (context) => UbahAkun(),
         Riwayat.routeName: (context) => Riwayat(),
         TentangKami.routeName: (context) => TentangKami(),
+        DetailKebab.routeName: (context) => DetailKebab(
+              detailImage: ModalRoute.of(context)?.settings.arguments as String,
+              detailPrice: ModalRoute.of(context)?.settings.arguments as int,
+              detailToping:
+                  ModalRoute.of(context)?.settings.arguments as String,
+              totalPriceSize: ModalRoute.of(context)?.settings.arguments as int,
+              typeSize: ModalRoute.of(context)?.settings.arguments as String,
+            ),
+        Alamat.routeName: (context) => Alamat(),
+        AlamatBaru.routeName: (context) => AlamatBaru(),
+        GoogleMapsScreen.routeName: (context) => GoogleMapsScreen(),
       },
     );
   }
 }
-
