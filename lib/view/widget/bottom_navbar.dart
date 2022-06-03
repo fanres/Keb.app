7d8c10b22fc52791efc46712cf6f1ae65cd0002b
Nurhafifah20 <nurhafifah265@gmail.com>
2022-06-03 19:58:23 +0700

    bottomnav fiks

--- lib/view/widget/bottom_navbar.dart
+++ lib/view/widget/bottom_navbar.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:kebapp/view/auth_user/ui/keranjang/keranjang.dart';
 import 'package:kebapp/view/auth_user/ui/menu/menu.dart';
